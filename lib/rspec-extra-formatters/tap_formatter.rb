9157d1377f9c998de95f15a6b32bb7e1af36130b
Rafael Souza <me@rafaelss.com>
2012-06-15 22:01:00 -0300

    add colors

--- lib/rspec-extra-formatters/tap_formatter.rb
+++ lib/rspec-extra-formatters/tap_formatter.rb
@@ -1,11 +1,11 @@
 # -*- encoding : utf-8 -*-
-# 
+#
 # Copyright (c) 2011, Diego Souza
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #   * Redistributions of source code must retain the above copyright notice,
 #     this list of conditions and the following disclaimer.
 #   * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 #   * Neither the name of the <ORGANIZATION> nor the names of its contributors
 #     may be used to endorse or promote products derived from this software
 #     without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,9 +26,9 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require "rspec/core/formatters/base_formatter"
+require "rspec/core/formatters/base_text_formatter"
 
-class TapFormatter < RSpec::Core::Formatters::BaseFormatter
+class TapFormatter < RSpec::Core::Formatters::BaseTextFormatter
 
   attr_reader :total
 
@@ -37,27 +37,26 @@ class TapFormatter < RSpec::Core::Formatters::BaseFormatter
   TODO   = '# TODO '
 
   def initialize(output)
-    super(output)      
+    super(output)
     @total = 0
   end
 
   def example_passed(example)
     super(example)
-    tap_example_output(OK, example)
+    tap_example_output(green(OK), example)
   end
 
   def example_pending(example)
     super(example)
-    tap_example_output(NOT_OK, example, TODO)
+    tap_example_output(yellow(NOT_OK), example, TODO)
   end
 
   def example_failed(example)
     super(example)
-    tap_example_output(NOT_OK, example)
+    tap_example_output(red(NOT_OK), example)
   end
 
   def dump_summary(duration, example_count, failure_count, pending_count)
-    super(duration, example_count, failure_count, pending_count)
     if (@total > 0)
       output.puts("1..#{example_count}")
     end
